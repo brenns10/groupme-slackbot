24da5c0eb30d6163473d05cfa8e788c71c2d8992
Stephen Brennan <smb196@case.edu>
2016-11-16 23:19:00 -0500

    Smaller zip for deploy

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,12 @@
 # Create zip package.
 rm package.zip
 zip package.zip bot.py config.py
-pushd venv/lib/python2.7/site-packages
-zip -r ../../../../package.zip *
+mkdir libs
+pip2 install -t libs requests
+pushd libs
+zip -r ../package.zip *
 popd
+rm -rf libs
 
 # Set your lambda function's code.
 aws lambda update-function-code \
